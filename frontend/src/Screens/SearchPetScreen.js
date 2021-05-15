f1517ede987d08eb04acf67641e469db07c13a31
kaanmertkoc <46743238+kaanmertkoc@users.noreply.github.com>
2021-05-15 23:29:17 +0300

    Register screen, login screen

---
+++ frontend/src/Screens/SearchPetScreen.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+import '../Styles/SearchPet.css';
+import { useDispatch, useSelector } from 'react-redux';
+
+const SearchPetScreen = () => {
+  const dispatch = useDispatch();
+
+  return <div></div>;
+};
+
+export default SearchPetScreen;
