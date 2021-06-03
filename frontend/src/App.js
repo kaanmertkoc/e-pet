924008944010aaabccec3c1d54d2c81eb334f705
Arif Dikkanat <61694750+ArifDKNT@users.noreply.github.com>
2021-06-03 21:48:17 +0300

    .

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import OwnerMainPageScreen from '../src/Screens/OwnerMainPageScreen';
 import RegisterScreen from '../src/Screens/RegisterScreen';
 import AdminScreen from '../src/Screens/AdminScreen';
 import Header from './Components/Header';
+import OwnerAppointmentPage from './Screens/OwnerAppointmentsPage';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="/register" exact component={RegisterScreen} />
         <Route path="/admin" exact component={AdminScreen} />
         <Route path="/owner" exact component={OwnerMainPageScreen} />
+        <Route path="/ownerAppointment" exact component={OwnerAppointmentPage} />
       </Switch>
     </Router>
   );
