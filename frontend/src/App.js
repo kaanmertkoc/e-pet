52c91d263f472c841777e858376230b927554a66
demetcaliskan <demetcaliskann@gmail.com>
2021-06-03 23:38:10 +0300

    vet main page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import LandingPageScreen from '../src/Screens/LandingPageScreen';
 import RegisterScreen from '../src/Screens/RegisterScreen';
 import AdminScreen from '../src/Screens/AdminScreen';
+import VetMainPageScreen from '../src/Screens/VetMainPageScreen';
 import Header from './Components/Header';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" exact component={LandingPageScreen} />
         <Route path="/register" exact component={RegisterScreen} />
         <Route path="/admin" exact component={AdminScreen} />
+        <Route path="/vet-main-page" exact component={VetMainPageScreen} />
       </Switch>
     </Router>
   );
---
+++ frontend/src/Screens/VetMainPageScreen.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { register } from '../actions/userActions';
+import { Form, Button, Alert } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import '../Styles/VetMainPageScreen.css';
+import cino from '../images/cino.jpeg';
+
+const VetMainPageScreen = () => {
+    return (
+        <div className="vet-main-div">
+
+            <div className="column">
+                <img className="pet-img" src={cino} alt="cino-dog" />
+                <div className="pet-short-info-div">
+                    <p className="pet-info-p">Cino</p>
+                    <p className="pet-info-p">123456789</p>
+                    <Button className="edit-btn"><h6>Edit</h6></Button>
+                </div>
+            </div>
+            <div className="column">
+                <img className="pet-img" src={cino} alt="cino-dog" />
+                <div className="pet-short-info-div">
+                    <p className="pet-info-p">Dino</p>
+                    <p className="pet-info-p">12345678910</p>
+                    <Button className="edit-btn"><h6>Edit</h6></Button>
+                </div>
+            </div>
+            <div className="column">
+                <img className="pet-img" src={cino} alt="cino-dog" />
+                <div className="pet-short-info-div">
+                    <p className="pet-info-p">Fino</p>
+                    <p className="pet-info-p">12345678911</p>
+                    <Button className="edit-btn"><h6>Edit</h6></Button>
+                </div>
+            </div>
+            <div className="column">
+                <img className="pet-img" src={cino} alt="cino-dog" />
+                <div className="pet-short-info-div">
+                    <p className="pet-info-p">Fino</p>
+                    <p className="pet-info-p">12345678911</p>
+                    <Button className="edit-btn"><h6>Edit</h6></Button>
+                </div>
+            </div>
+
+
+        </div>
+    );
+}
+
+export default VetMainPageScreen;
