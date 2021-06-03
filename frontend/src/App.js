f9cd674de3dfb6c8e51e7157918ef952bdc92f0d
kaanmertkoc <46743238+kaanmertkoc@users.noreply.github.com>
2021-06-03 23:34:26 +0300

    Search screen done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import LandingPageScreen from '../src/Screens/LandingPageScreen';
 import RegisterScreen from '../src/Screens/RegisterScreen';
 import AdminScreen from '../src/Screens/AdminScreen';
 import Header from './Components/Header';
+import SearchPetScreen from './Screens/SearchPetScreen';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" exact component={LandingPageScreen} />
         <Route path="/register" exact component={RegisterScreen} />
         <Route path="/admin" exact component={AdminScreen} />
+        <Route path="/search/:id" exact component={SearchPetScreen} />
       </Switch>
     </Router>
   );
