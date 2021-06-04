6b49b3918d66ba4a1603c0064c6f3611c38a19f4
kaanmertkoc <46743238+kaanmertkoc@users.noreply.github.com>
2021-06-04 23:57:39 +0300

    Add pet screen complete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import RegisterScreen from '../src/Screens/RegisterScreen';
 import AdminScreen from '../src/Screens/AdminScreen';
 import Header from './Components/Header';
 import SearchPetScreen from './Screens/SearchPetScreen';
+import AddPetScreen from './Screens/AddPetScreen';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="/register" exact component={RegisterScreen} />
         <Route path="/admin" exact component={AdminScreen} />
         <Route path="/search/:id" exact component={SearchPetScreen} />
+        <Route path="/addPet" exact component={AddPetScreen} />
       </Switch>
     </Router>
   );
