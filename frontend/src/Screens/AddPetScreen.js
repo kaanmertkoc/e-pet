eb506b4158f38a897bfd947eb58b50929577bbae
kaanmertkoc <46743238+kaanmertkoc@users.noreply.github.com>
2021-06-06 00:02:09 +0300

    Add pet screen finish

--- frontend/src/Screens/AddPetScreen.js
+++ frontend/src/Screens/AddPetScreen.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import '../Styles/AddPet.css';
+import { Form } from 'react-bootstrap';
 
 const AddPetScreen = () => {
+  const [name, setName] = useState('');
+  const [date, setDate] = useState('');
+  const [race, setRace] = useState('');
+  const [sex, setSex] = useState('');
+  const [colour, setColour] = useState('');
+  const [vaccines, setVaccines] = useState({});
+  const [ownerName, setOwnerName] = useState('');
+  const [phone, setPhone] = useState('');
+  const [email, setEmail] = useState('');
+  const [address, setAdress] = useState('');
+
+  const handleChange = event => {
+    setVaccines({ ...vaccines, [event.target.name]: event.target.checked });
+  };
+
   return (
     <div className="back">
       <div className="add-pet-container">
@@ -30,54 +47,94 @@ const AddPetScreen = () => {
             <div className="pet-info">
               <h6>Name: </h6>
               <h6>Date of Birth: </h6>
-              <h6>Breed: </h6>
+              <h6>Race: </h6>
               <h6>Sex: </h6>
               <h6>Colour: </h6>
               <h6>Vaccinations: </h6>
             </div>
             <div className="pet-details">
-              <h6>Cino </h6>
-              <h6>28.10.2010</h6>
-              <h6>Golden Retreiver</h6>
-              <h6>Male</h6>
-              <h6>Light Blonde</h6>
-              <div className="vaccine-row">
-                <div className="vaccine">
-                  <h6>Vaccine 1</h6>
-                  <img
-                    src="/check-circle-solid.svg"
-                    alt="something"
-                    width="30px"
-                    height="30px"
-                  />
-                </div>
-                <div className="vaccine">
-                  <h6>Vaccine 2</h6>
-                  <img
-                    src="/check-circle-solid.svg"
-                    alt="something"
-                    width="30px"
-                    height="30px"
-                  />
-                </div>
-                <div className="vaccine">
-                  <h6>Vaccine 3</h6>
-                  <img
-                    src="/check-circle-solid.svg"
-                    alt="something"
-                    width="30px"
-                    height="30px"
-                  />
-                </div>
-                <div className="vaccine">
-                  <h6>Vaccine 4</h6>
-                  <img
-                    src="/check-circle-solid.svg"
-                    alt="something"
-                    width="30px"
-                    height="30px"
-                  />
-                </div>
+              <Form.Group controlId="nameInput">
+                <Form.Control
+                  type="name"
+                  value={name}
+                  onChange={e => setName(e.target.value)}
+                ></Form.Control>
+              </Form.Group>
+              <Form.Group
+                style={{ margin: '5px 0px 0px 0px' }}
+                controlId="dateOfBirth"
+              >
+                <Form.Control
+                  type="date"
+                  onChange={e => setDate(e.target.value.toString())}
+                ></Form.Control>
+              </Form.Group>
+              <Form.Group
+                style={{ margin: '5px 0px 0px 0px' }}
+                controlId="breedInput"
+              >
+                <Form.Control
+                  type="breed"
+                  value={race}
+                  onChange={e => setRace(e.target.value)}
+                ></Form.Control>
+              </Form.Group>
+              <Form.Group
+                style={{ margin: '5px 0px 0px 0px' }}
+                controlId="sexInput"
+              >
+                <Form.Control
+                  type="sex"
+                  value={sex}
+                  onChange={e => setSex(e.target.value)}
+                ></Form.Control>
+              </Form.Group>
+              <Form.Group
+                style={{ margin: '5px 0px 0px 0px' }}
+                controlId="colourInput"
+              >
+                <Form.Control
+                  type="colour"
+                  value={colour}
+                  onChange={e => setColour(e.target.value)}
+                ></Form.Control>
+              </Form.Group>
+              <div
+                style={{ margin: '20px 0px 0px 0px' }}
+                className="vaccine-row"
+              >
+                <Form.Check
+                  inline
+                  label="Vaccine1"
+                  name="vaccine1"
+                  type="radio"
+                  checked={vaccines['vaccine1']}
+                  onChange={handleChange}
+                />
+                <Form.Check
+                  inline
+                  label="Vaccine2"
+                  name="vaccine2"
+                  type="radio"
+                  checked={vaccines['vaccine2']}
+                  onChange={handleChange}
+                />
+                <Form.Check
+                  inline
+                  label="Vaccine3"
+                  name="vaccine3"
+                  type="radio"
+                  checked={vaccines['vaccine3']}
+                  onChange={handleChange}
+                />
+                <Form.Check
+                  inline
+                  label="Vaccine4"
+                  name="vaccine4"
+                  type="radio"
+                  checked={vaccines['vaccine4']}
+                  onChange={handleChange}
+                />
               </div>
             </div>
           </div>
@@ -90,10 +147,43 @@ const AddPetScreen = () => {
               <h6>Adress: </h6>
             </div>
             <div className="pet-details">
-              <h6>Arif Dikkanat </h6>
-              <h6>+905324587887</h6>
-              <h6>dikkanata@hayvansever.com</h6>
-              <h6>Earth</h6>
+              <Form.Group controlId="ownerNameInput">
+                <Form.Control
+                  type="name"
+                  value={ownerName}
+                  onChange={e => setOwnerName(e.target.value)}
+                ></Form.Control>
+              </Form.Group>
+              <Form.Group
+                style={{ margin: '10px 0px 0px 0px' }}
+                controlId="phoneInput"
+              >
+                <Form.Control
+                  type="phone"
+                  value={phone}
+                  onChange={e => setPhone(e.target.value)}
+                ></Form.Control>
+              </Form.Group>
+              <Form.Group
+                style={{ margin: '10px 0px 0px 0px' }}
+                controlId="emailInput"
+              >
+                <Form.Control
+                  type="email"
+                  value={email}
+                  onChange={e => setEmail(e.target.value)}
+                ></Form.Control>
+              </Form.Group>
+              <Form.Group
+                style={{ margin: '10px 0px 0px 0px' }}
+                controlId="addressInput"
+              >
+                <Form.Control
+                  type="name"
+                  value={address}
+                  onChange={e => setAdress(e.target.value)}
+                ></Form.Control>
+              </Form.Group>
             </div>
           </div>
         </div>
