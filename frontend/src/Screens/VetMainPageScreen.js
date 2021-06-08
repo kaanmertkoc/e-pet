3f59ebbfc7bf466e808340108b5a487ff0884510
demetcaliskan <demetcaliskann@gmail.com>
2021-06-08 16:19:37 +0300

    vet-main-page

--- frontend/src/Screens/VetMainPageScreen.js
+++ frontend/src/Screens/VetMainPageScreen.js
@@ -3,22 +3,22 @@ import { useDispatch, useSelector } from 'react-redux';
 import { register } from '../actions/userActions';
 import { Form, Button, Alert } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import '../Styles/VetMainPageScreen.css';
 import cino from '../images/cino.jpeg';
+import '../Styles/VetMainPageScreen.css';
 
 const VetMainPageScreen = () => {
     return (
         <div className="vet-main-div">
             <div className="header-patient">
                 <h1>Patients</h1>
-                <Button className="edit-btn"><h6>Add Patient</h6></Button>
+                <Button className="edit-btn">Add Patient</Button>
             </div>
             <div className="column">
                 <img className="pet-img" src={cino} alt="cino-dog" />
                 <div className="pet-short-info-div">
                     <p className="pet-info-p">Cino</p>
                     <p className="pet-info-p">123456789</p>
-                    <Button className="edit-btn"><h6>Edit</h6></Button>
+                    <Button className="edit-btn">Edit</Button>
                 </div>
             </div>
             <div className="column">
@@ -26,7 +26,7 @@ const VetMainPageScreen = () => {
                 <div className="pet-short-info-div">
                     <p className="pet-info-p">Dino</p>
                     <p className="pet-info-p">12345678910</p>
-                    <Button className="edit-btn"><h6>Edit</h6></Button>
+                    <Button className="edit-btn">Edit</Button>
                 </div>
             </div>
             <div className="column">
@@ -34,7 +34,7 @@ const VetMainPageScreen = () => {
                 <div className="pet-short-info-div">
                     <p className="pet-info-p">Fino</p>
                     <p className="pet-info-p">12345678911</p>
-                    <Button className="edit-btn"><h6>Edit</h6></Button>
+                    <Button className="edit-btn">Edit</Button>
                 </div>
             </div>
             <div className="column">
@@ -42,7 +42,7 @@ const VetMainPageScreen = () => {
                 <div className="pet-short-info-div">
                     <p className="pet-info-p">Fino</p>
                     <p className="pet-info-p">12345678911</p>
-                    <Button className="edit-btn"><h6>Edit</h6></Button>
+                    <Button className="edit-btn">Edit</Button>
                 </div>
             </div>
 
